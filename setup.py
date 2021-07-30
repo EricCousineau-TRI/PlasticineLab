820f5c7e671069f25dcdced2c29dc0d3230654a2
Eric Cousineau <eric.cousineau@tri.global>
2021-07-30 15:48:44 -0400

    hack in better baselines version

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
 
-install_requires = ['scipy', 'numpy', 'torch', 'opencv-python', 'tqdm', 'taichi', 'gym', 'tensorboard', 'yacs', 'baselines']
+install_requires = [
+      'scipy', 'numpy', 'torch', 'opencv-python', 'tqdm', 'taichi', 'gym', 'tensorboard', 'yacs',
+      # HACK: See https://github.com/hzaskywalker/PlasticineLab/issues/3
+      'git+https://github.com/openai/baselines@ea25b9e8',
+]
 
 setup(name='plb',
       version='0.0.1',
